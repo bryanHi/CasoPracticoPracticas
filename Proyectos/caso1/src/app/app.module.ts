80da9b2558bd077112eebd8ec794b07f7a7e44a0
bryanHi <56934371+bryanHi@users.noreply.github.com>
2020-12-08 23:55:01 -0500

    Login

--- Proyectos/caso1/src/app/app.module.ts
+++ Proyectos/caso1/src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule, 
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
       { path: 'home', component: HomeComponent },
     ]),
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Proyectos/caso1/src/app/login/login.component.ts
+++ Proyectos/caso1/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../services/http/http.service'
+import { HttpService } from '../services/http/http.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,11 +8,49 @@ import { HttpService } from '../services/http/http.service'
 })
 export class LoginComponent implements OnInit {
 
+  correo: any;
+  clave: any;
+
   constructor(
     public http: HttpService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
   }
+
+  login() {
+    let body = {
+      correo: this.correo,
+      clave: this.clave,
+    }
+
+    this.http.postPromise('login', body, this.http.postHeaderEmpty('application/json')).then(
+      (res: any) => {
+        console.log(res);
+        if (res.length > 0) {
+          this.router.navigate(['/home']);
+        } else {
+          alert("Usuario o clave incorrecta");
+        }
+      },
+      err => {
+
+      }
+    )
+  }
+
+  test() {
+    this.http.getPromise('test', '').then(
+      res => {
+        console.log(res);
+
+      },
+      err => {
+
+      }
+    );
+  }
+
 
 }
